87fa660944076a565a07b4f81fd51a92f098f014
Michał Kulig <kulia16@gmail.com>
2016-03-21 13:21:33 +0100

    add blax box test

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -44,7 +44,7 @@ module.exports = function (config)
         // - Safari (only Mac)
         // - PhantomJS
         // - IE (only Windows)
-        browsers: ['PhantomJS'],
+        browsers: ['PhantomJS','Chrome'],
 
         // Which plugins to enable
         plugins: ['karma-phantomjs-launcher', 'karma-jasmine', 'karma-spec-reporter', 'karma-junit-reporter', 'karma-coverage'],
